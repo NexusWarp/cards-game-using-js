376aa220d27867b5fb24cf73984724b6a4da9788
NexusWarp <ik571572@gmail.com>
2023-12-31 17:39:22 +0500

    update

--- function.js
+++ function.js
@@ -1,51 +1,66 @@
 let cardsArray = [];
 let score = 0;
 let turns = 3;
-let ranCrd = (Math.floor(Math.random()*52)) + 1;
-let CreateImage = `<img id="crd" src="images/${ranCrd}.png" class="${ranCrd}">`;
-let currentLevel = 1;
+// let ranCrd = (Math.floor(Math.random()*52)) + 1;
+// let CreateImage = `<img id="crd" src="images/${ranCrd}.png" class="${ranCrd}">`;
+let currentLevel = 10;
 document.querySelector(".curlevel").innerHTML = currentLevel;
-cardsArray.push(ranCrd);
+// cardsArray.push(ranCrd);
 
 
-
-for(i=1;i<currentLevel;i++){
-    let rn = (Math.floor(Math.random()*52))+1;
-    cardsArray.push(rn);
-    CreateImage += `<img id="crd" src="images/${rn}.png" class="${rn}">`;
+//create random cards
+function ImageCreation(level){
+    let CreateImage ="";
+    for(i=0;i<level;i++){
+        let rn = (Math.floor(Math.random()*52))+1;
+        cardsArray.push(rn);
+        CreateImage += `<img id="crd" src="images/${rn}.png" class="${rn}">`;
+    }
+    document.querySelector(".crd").innerHTML = CreateImage;
 }
+ImageCreation(currentLevel);
+
+let rmCard;
+function CreateTargetCard(arrLength){
+    let randomCrd = (Math.floor(Math.random()*arrLength));
+    rmCard = cardsArray[randomCrd];
+    let SelectedCrd =  `<img src="images/${rmCard}.png" class="${rmCard}.">`
+     document.querySelector("#rncard").innerHTML = SelectedCrd;
+}
+CreateTargetCard(cardsArray.length);
+
+
+// console.log(cardsArray);
+// for(i=1;i<currentLevel;i++){
+//     let rn = (Math.floor(Math.random()*52))+1;
+//     cardsArray.push(rn);
+//     CreateImage += `<img id="crd" src="images/${rn}.png" class="${rn}">`;
+// }
 
 // console.log(cardsArray.length);
-let randomCrd = (Math.floor(Math.random()*cardsArray.length));
-let rmCard = cardsArray[randomCrd];
-let SelectedCrd =  `<img src="images/${rmCard}.png" class="${rmCard}.">`
- document.querySelector("#rncard").innerHTML = SelectedCrd;
 
 
-document.querySelector(".crd").innerHTML = CreateImage;
 
-document.querySelector(".gamepanel").addEventListener("click",(det)=>{
+
+//check clciked card
+document.querySelector(".crd").addEventListener("click",(det)=>{
 let chosedCrd = det.target.className;
 if(chosedCrd == rmCard){
     score+=10;
+    currentLevel++;
     document.querySelector(".gamescore").innerHTML = score;
 }
-else{
+else if(chosedCrd !== rmCard){
     turns --;
    
     document.querySelector(".turnsrem").innerHTML = turns;
 }
 })
 
+
+//next level
 document.querySelector("#nextbtn").addEventListener("click",()=>{
-    currentLevel++;
-    alert("clicked " + currentLevel);
-    for(i=1;i<currentLevel;i++){
-        let rn = (Math.floor(Math.random()*52))+1;
-        cardsArray.push(rn);
-        console.log(rn);
-        CreateImage += `<img id="crd" src="images/${rn}.png" class="${rn}">`;
-        
-    }
-    document.querySelector(".crd").innerHTML = CreateImage;
+    cardsArray = [];
+    ImageCreation(currentLevel);
+    
 })
